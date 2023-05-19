1eaa34270ad3212e7cb699c350c79a645a3c0042
Marilyn V <56269153+MxrilynV@users.noreply.github.com>
2023-05-19 09:52:27 -0700

    Just added a few comments and changed the webpage color.

--- Server/mainServer.js
+++ Server/mainServer.js
@@ -6,7 +6,9 @@ const startWebSocketServer = require('./webSocketServer.js');
 const httpServer = startHttpServer();
 const webSocketServer = startWebSocketServer(httpServer);
 
-webSocketServer.on('upgrade', (request, socket, head) => {
+// Handle upgrade requests for websockets
+// Event is triggered when a client requests a HTTP upgrade to a websocket one
+webSocketServer.on('upgrade', (request, socket, head) => { //incoming http request, socket (IP address & port), head (first packet of the upgraded stream)
   webSocketServer.handleUpgrade(request, socket, head, (ws) => {
     webSocketServer.emit('connection', ws, request);
   });
--- Server/networkConfig.js
+++ Server/networkConfig.js
@@ -1,5 +1,5 @@
-let global_IP ='10.0.0.181' // Home IP
-//let global_IP = '10.143.127.26'; // School IP
+//let global_IP ='10.0.0.181' // Home IP
+let global_IP = '10.143.127.26'; // School IP
 let ws_port = 4000;
 let http_port = 4001;
 
